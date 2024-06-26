5322c37cf6b35779624f3891ed5c22f3ad037471
EunbeenDev <ros4312@naver.com>
2024-06-26 23:15:24 +0900

    15주차 문제 풀이

---
+++ 노은빈/week1/1417.국회의원 선거.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+        int Dasom = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        // pq.add(Dasom);
+        if(N>1){
+            for(int i=1; i<N; i++){
+                pq.add(Integer.parseInt(br.readLine()));
+            }
+
+            while(pq.peek() >= Dasom){
+                int max = pq.poll();
+                Dasom++;
+                max--;
+                pq.add(max);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
